e2342ef4cb7aacfc7fb8f8b9cac75347b2b9f3a8
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-10-26 16:35:25 -0400

    new: plot zip code frequency data
    Write function to plot zip code frequency on a US map

--- location.py
+++ location.py
@@ -1,4 +1,9 @@
+import geopandas
+import matplotlib.pyplot as plt
+import pandas as pd
+import pandas_bokeh
 import pycountry
+
 from data import g
 
 
@@ -19,4 +24,18 @@ def tally_countries(ip):
 
 
 def plot_postal_codes():
-    pass
+    pandas_bokeh.output_notebook()
+    us = geopandas.read_file("./zip_map/cb_2018_us_zcta510_500k.shp")
+
+    # Plot Zip Code Frequencies
+    df=pd.DataFrame({'PCODE': list(g.postal_code.keys()), 'A':list(g.postal_code.values()) })
+
+    # Join Zip Code Plot to US Plot
+    new_df=us.join(df.set_index('PCODE'), on='ZCTA5CE10')
+
+
+    new_df.plot_bokeh(simplify_shapes=20000,
+                      category="A",
+                      colormap="Spectral",
+                      hovertool_columns=["ZCTA5CE10","A"])
+
